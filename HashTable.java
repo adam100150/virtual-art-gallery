a5e263a11976a5c1dec7fad933ea9e30a06977ee
adam100150 <adam100150@gmail.com>
2021-06-23 14:29:03 -0700

    Add files via upload

---
+++ HashTable.java
@@ -0,0 +1,253 @@
+/**
+ * HashTable.java
+ * @artist Nathan Brin
+ * @artist Adam Ashkenazi
+ * @artist Sihan Sun
+ * @artist Alice Zhang
+ * CIS 22C Final Project
+ */
+
+
+import java.util.ArrayList;
+
+public class HashTable<T> {
+
+	private int numElements;
+	private ArrayList<List<T> > Table;
+
+
+    /**
+     * Constructor for the hash
+     * table. Initializes the Table to
+     * be sized according to value passed
+     * in as a parameter
+     * Inserts size empty Lists into the
+     * table. Sets numElements to 0
+     * @param size the table size
+     */
+    public HashTable(int size) {
+    	Table = new ArrayList<List<T>>();
+        for (int i = 0; i < size; i++) {
+        	Table.add(new List<T>());
+        }
+        numElements = 0;
+    }
+
+
+	/**Accessors*/
+
+    /**
+     * returns the hash value in the Table
+     * for a given Object
+     * @param t the Object
+     * @return the index in the Table
+     */
+    private int hash(T t) {
+        int code = t.hashCode();
+        return code % Table.size();
+    }
+
+	/**
+	 * Counts the number of elements at this index
+	 * @param index the index in the Table
+	 * @precondition 0 <= index < Table.length
+	 * @return the count of elements at this index
+	 * @throws IndexOutOfBoundsException
+	 */
+	public int countBucket(int index) throws IndexOutOfBoundsException {
+		if (index < 0 || index > Table.size()) {
+			throw new IndexOutOfBoundsException("countBucket(): index is out of bounds");
+		} else {
+			return Table.get(index).getLength();
+		}
+	}
+
+	/**
+	 * Returns total number of elements in the Table
+	 * @return total number of elements
+	 */
+	public int getNumElements() {
+		return numElements;
+	}
+
+	/**
+	 * Accesses a specified element in the Table
+	 * @param t the element to search for
+	 * @return the element stored in the Table,
+	 * or null if this Table does not contain t.
+	 * @precondition t != null
+	 * @throws NullPointerException when t is null
+	 */
+	public T get(T t) throws NullPointerException{
+		if (t == null) {
+			throw new NullPointerException("get(): t is null");
+		} else {
+			int index = hash(t);			
+			List<T> temp = Table.get(index);
+			if (temp == null){
+				return null;
+			}
+
+			int listIndex = temp.linearSearch(t);				
+			if (listIndex != -1) {
+				temp.iteratorToIndex(listIndex);
+				return temp.getIterator();
+			}
+			return null;
+		}
+	}
+
+    /**
+     * Determines whether a specified key is in
+     * the Table
+     * @param t the element to search for
+     * @return  whether the element is in the Table
+     * @precondition t != null
+     * @throws NullPointerException when t is null
+     */
+	 /**
+     * Determines whether a specified key is in
+     * the Table
+     * @param t the element to search for
+     * @return  whether the element is in the Table
+     * @precondition t != null
+     * @throws NullPointerException when t is null
+     */
+    public boolean contains(T t) throws NullPointerException{
+        if (t == null) {
+        	throw new NullPointerException("contains(): t is null");
+        } else {
+        	int index = hash(t);
+        	List<T> temp = Table.get(index);
+			if (temp == null)
+			{
+				return false;
+			}
+        	return temp.linearSearch(t) != -1;
+        }
+    }
+
+
+
+	/**Mutators*/
+
+    /**
+     * Inserts a new element in the Table
+     * at the end of the chain in the bucket
+     * @param t the element to insert
+     * @precondition t != null
+     * @throws NullPointerException when t is null
+     */
+	public void insert(T t) throws NullPointerException{
+		if (t == null) {
+			throw new NullPointerException("insert(): t is null");
+		} else {
+			int index = hash(t);			
+			Table.get(index).addLast(t);
+			numElements++;
+		}
+	}
+
+
+	/**
+     * removes the key t from the Table
+     * calls the hash method on the key to
+     * determine correct placement
+     * has no effect if t is not in
+     * the Table or for a null argument
+     * @param t the key to remove
+     * @throws NullPointerException when t is null
+     */
+	public void remove(T t) {
+		if (t == null) {
+			throw new NullPointerException("remove(): t is null");
+		} else {
+			int index = hash(t);
+			List<T> temp = Table.get(index);
+			if (temp == null) {
+				return;
+			}
+
+			int listIndex = temp.linearSearch(t);
+			if (listIndex == -1) {
+				return;
+			}			
+
+			temp.iteratorToIndex(listIndex);
+			temp.removeIterator();
+		}
+	}
+
+	/**
+     * Clears this hash table so that it contains no keys.
+     */
+    public void clear() {
+    	for (int i = 0; i < Table.size(); i++) {
+    		Table.set(i, new List<T>());
+    	}
+    }
+
+	/**Additional Methods*/
+
+	/**
+	 * Prints all the keys at a specified
+	 * bucket in the Table. Each key displayed
+	 * on its own line, with a blank line
+	 * separating each key
+	 * Above the keys, prints the message
+	 * "Printing bucket #<bucket>:"
+	 * Note that there is no <> in the output
+	 * @param bucket the index in the Table
+	 * @throws IndexOutOfBoundsException
+	 */
+	public void printBucket(int bucket) throws IndexOutOfBoundsException {
+		if (bucket < 0 || bucket > Table.size()) {
+			throw new IndexOutOfBoundsException("printBucket(): index is out of bounds");
+		} else {
+			List<T> temp = Table.get(bucket);
+			System.out.println("Printing bucket #" + bucket + ":\n");
+			System.out.println(temp);			
+		}
+
+	}
+
+	/**
+	 * Prints the first key at each bucket
+	 * along with a count of the total keys
+	 * with the message "+ <count> -1 more
+	 * at this bucket." Each bucket separated
+	 * with two blank lines. When the bucket is
+	 * empty, prints the message "This bucket
+	 * is empty." followed by two blank lines
+	 */
+	public void printTable(){
+		for (int i = 0; i < Table.size(); i++) {
+			List<T> temp = Table.get(i);
+			System.out.println("Bucket: " + i);
+			if (temp.isEmpty()){
+				System.out.println("This bucket is empty.\n");
+			} else {
+				System.out.println(temp.getFirst());
+				System.out.println("+ " + (temp.getLength()-1) + " more at this bucket\n");
+				System.out.println();
+			}
+		}
+	}
+
+	/**
+	 * Starting at the first bucket, and continuing
+	 * in order until the last bucket, concatenates
+	 * all elements at all buckets into one String
+	 */
+	@Override public String toString() {
+		String s = "";
+		if (numElements == 0) {
+			return s;
+		}
+		
+		for (int i = 0; i < Table.size(); i++) {
+			s += Table.get(i).toString();			
+		}
+		return s;
+	}
+}
---
+++ Store.java
@@ -0,0 +1,308 @@
+/**
+ * Store.java
+ * @artist Nathan Brin
+ * @artist Adam Ashkenazi
+ * @artist Sihan Sun
+ * @artist Alice Zhang
+ * CIS 22C Final Project
+ */
+import java.util.*;
+import java.io.*;
+
+public class Store
+{
+	private  final int NUM_CUSTOMERS = 7;
+	private  final int NUM_ORDERS = 100;
+	private PriorityComparator pc;
+
+	private static BST<Painting> painting_name = new BST<>();
+	private  static BST<Painting> painting_value = new BST<>();
+	private  static HashTable<Customer> customers;
+	private Heap<Order> ordersStandard;
+	private Heap<Order> ordersRushed;
+	private Heap<Order> ordersOvernight;
+
+	private List<Order> unshippedOrders;
+	private List<Order> shippedOrders;
+
+	private NameComparator nC;
+	private ValueComparator vC;
+
+	public Store()
+	{
+		pc = new PriorityComparator();
+		nC = new NameComparator();
+		vC = new ValueComparator();
+		customers = new HashTable<>(NUM_CUSTOMERS);
+		ordersStandard = new Heap<>(NUM_ORDERS, pc);
+		ordersRushed = new Heap<>(NUM_ORDERS, pc);
+		ordersOvernight = new Heap<>(NUM_ORDERS, pc);
+
+		unshippedOrders = new List<>();
+		shippedOrders = new List<>();
+
+		try {
+			buildPaintings();
+			readCustomersFile();
+			buildOrders();
+		}catch(FileNotFoundException e) {
+			System.out.println("Could not find file.");
+			e.printStackTrace();
+		}
+	}
+
+	public void buildPaintings() throws FileNotFoundException {
+		String title, artist, description;
+		double price;
+		int year;
+
+		File file = new File("Paintings.txt");
+		Scanner input = new Scanner(file);
+		while(input.hasNextLine()) {
+			title = input.nextLine();
+			artist = input.nextLine();
+			year = input.nextInt();
+			input.nextLine();
+			price = input.nextFloat();
+			input.nextLine();
+			description = input.nextLine();
+			if(input.hasNextLine()) {
+				input.nextLine();
+			}
+			painting_name.insert(new Painting(title, artist, year, price, description), nC);
+			painting_value.insert(new Painting(title, artist, year, price, description), vC);
+		}
+		input.close();
+	}
+
+	public void readCustomersFile() throws FileNotFoundException{
+
+		String userName, password, firstName, lastName, email, address;
+		double cash;
+
+		File file = new File("Customers.txt");
+		Scanner input = new Scanner(file);
+		while(input.hasNextLine()) {
+			userName = input.nextLine();
+			password = input.nextLine();
+			firstName = input.next();
+			lastName = input.next();
+			input.nextLine();
+
+			email = input.nextLine();
+			address = input.nextLine();
+			cash = input.nextDouble();
+
+
+			if(input.hasNextLine()) {
+				input.nextLine();
+			}
+			customers.insert(new Customer(userName, password, firstName, lastName, email, address, cash));
+		}
+		input.close();
+	}
+
+	public void buildOrders() throws FileNotFoundException {
+		String user, password, painting, date;
+		int speed;
+
+		File file = new File("Orders.txt");
+		Scanner input = new Scanner(file);
+		while(input.hasNextLine())
+		{
+			user = input.nextLine();
+			password = input.nextLine();
+			date = input.nextLine();
+			painting = input.nextLine();
+			speed = input.nextInt();
+			if(input.hasNextLine())
+				input.nextLine();
+			Customer tempCust = customers.get(new Customer(user, password));
+			Painting tempPaint = painting_name.search(new Painting(painting), nC);
+			if(speed == 1)
+				ordersStandard.insert(new Order(tempCust, date, tempPaint, speed));
+			else if(speed == 2)
+				ordersRushed.insert(new Order(tempCust, date, tempPaint, speed));
+			else if(speed == 3)
+				ordersOvernight.insert(new Order(tempCust, date, tempPaint, speed));
+			unshippedOrders.addLast(new Order(tempCust, date, tempPaint, speed));
+		}
+		input.close();
+	}
+	public Painting searchPaintingName(Painting painting)
+	{
+		return painting_name.search(painting, nC);
+	}
+	public Painting searchPaintingPrice(Painting painting)
+	{
+		return painting_name.search(painting, vC);
+	}
+	public void printPaintingsByName()
+	{
+		painting_name.inOrderPrint();
+	}
+
+	public void printPaintingsByValue()
+	{
+		painting_value.inOrderPrint();
+	}
+
+	public Customer searchCustomer(Customer cust)
+	{
+		return customers.get(cust);
+	}
+	public boolean containsCustomer(Customer Cust)
+	{
+		return customers.contains(Cust);
+	}
+	public void displayCustomer(Customer cust)
+	{
+		System.out.print(customers.get(cust));
+	}
+	public void displayCustomers()
+	{
+		customers.printTable();
+	}
+	public void addCustomer(Customer cust)
+	{
+		customers.insert(cust);
+	}
+	public void placeOrder(Order order)
+	{
+		Customer temp = order.getCustomer();
+		if (temp.getCash() < order.getOrderContents().getPrice()) {
+			System.out.println("Customer doesn't have enough money in account");
+		} else {
+			if(order.getShippingSpeed() == 3)
+			{
+				ordersOvernight.insert(order);
+			}
+			else if(order.getShippingSpeed() == 2)
+			{
+				ordersRushed.insert(order);
+			}
+			else if(order.getShippingSpeed() == 1)
+			{
+				ordersStandard.insert(order);
+			}
+
+			Painting tempPainting = order.getOrderContents();
+			temp.addPainting(tempPainting);
+			Double price = tempPainting.getPrice();
+			temp.updateCash(-price);
+
+			unshippedOrders.addLast(order);
+		}
+	}
+
+	public void shipOrder()
+	{
+		if(!ordersOvernight.isEmpty())
+		{
+			Order currentOrder = ordersOvernight.pop();
+			Customer currentCust = currentOrder.getCustomer();
+			currentCust.addPainting(currentOrder.getOrderContents());
+			System.out.println("\nOrder Shipped: " + currentOrder);
+			shippedOrders.addLast(currentOrder);
+			unshippedOrders.removeFirst();
+		}
+		else if(!ordersRushed.isEmpty())
+		{
+			Order currentOrder = ordersRushed.pop();
+			Customer currentCust = currentOrder.getCustomer();
+			currentCust.addPainting(currentOrder.getOrderContents());
+			System.out.println("\nOrder Shipped: " + currentOrder);
+			shippedOrders.addLast(currentOrder);
+			unshippedOrders.removeFirst();
+		}
+		else if(!ordersStandard.isEmpty())
+		{
+			Order currentOrder = ordersStandard.pop();
+			Customer currentCust = currentOrder.getCustomer();
+			currentCust.addPainting(currentOrder.getOrderContents());
+			System.out.println("\nOrder Shipped: " + currentOrder);
+			shippedOrders.addLast(currentOrder);
+			unshippedOrders.removeFirst();
+		}
+		else
+			System.out.println("No orders to ship.");
+	}
+
+	public void viewUnshippedOrders()
+	{
+		if(unshippedOrders.isEmpty())
+			System.out.println("No orders to show.");
+		else
+		{
+			ArrayList<Order> list1 = ordersOvernight.sort();
+			ArrayList<Order> list2 = ordersRushed.sort();
+			ArrayList<Order> list3 = ordersStandard.sort();
+			list1.addAll(list2);
+			list1.addAll(list3);
+			System.out.println(list1);
+		}
+	}
+
+	public void viewShippedOrders()
+	{
+		if(shippedOrders.isEmpty())
+			System.out.println("No orders to show.");
+		else
+			shippedOrders.printNumberedList();
+	}
+
+	public void viewUnshippedOrders(Customer cust)
+	{
+		if(unshippedOrders.isEmpty())
+		{
+			System.out.println("No orders to show.");
+		}
+		else
+		{
+			List<Order> temp = new List<>(); //temp is the list of orders the Customer placed
+			unshippedOrders.placeIterator();
+			for(int i = 1; i <=unshippedOrders.getLength(); i++)
+			{
+				Order tempOrder = unshippedOrders.getIterator();
+				if(tempOrder.getCustomer().equals(cust))
+					temp.addLast(tempOrder);
+				unshippedOrders.advanceIterator();
+			}
+			temp.printNumberedList();
+		}
+	}
+
+	public void viewShippedOrders(Customer cust)
+	{
+		if(shippedOrders.isEmpty())
+		{
+			System.out.println("No orders to show.");
+		}
+		else
+		{
+			List<Order> temp = new List<>();
+			shippedOrders.placeIterator();
+			for(int i = 1; i <= shippedOrders.getLength(); i++)
+			{
+				Order tempOrder = shippedOrders.getIterator();
+				if(tempOrder.getCustomer().equals(cust))
+					temp.addLast(tempOrder);
+				shippedOrders.advanceIterator();
+			}
+			temp.printNumberedList();
+		}
+	}
+	public void addPainting(Painting painting)
+	{
+		painting_name.insert(painting, nC);
+		painting_value.insert(painting, vC);
+	}
+
+	public void removePainting(Painting painting)
+	{
+		System.out.println("Painting removed: " + painting);
+		painting_name.remove(painting, nC);
+		painting_value.remove(painting, vC);
+
+	}
+}
